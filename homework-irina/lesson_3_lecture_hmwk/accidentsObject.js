e3c02cf19987bd54e4243f562733d2037e0d6c2a
irina <i.mkhitaryan8@mail.ru>
2017-04-21 16:16:21 +0400

    second commit

--- homework-irina/lesson_3_lecture_hmwk/accidentsObject.js
+++ homework-irina/lesson_3_lecture_hmwk/accidentsObject.js
@@ -1,31 +1,39 @@
 var accidentTransportation = {
-		date : '12.03.2017',
-		listTranspotationInvolved : {
-		CivilianAviation: ['Boing737' , 'Airbus A319'],
-		MilitaryAviation: ['MIG-29' , 'AH-64 Apach']
-		},
-		DamagedPeopleNumber : 10,
-		reasonAccident : "The accident happened because the driver of Fiat was drunk.",
-		accidentInfo:function(){
-			return 'Crash happened on'  + this.date  + ' . ' +  this.reasonAccident  +  '.'  +  'damaged people number is' + this.DamagedPeopleNumber;
-		}
-		hasInfo:function(){
-				return this.hasOwnProperty();
-		}
-	} 
-	
-	var hasInfo=function(accidentRepresents){
-	 return accidentTransportation.hasOwnProperty(accidentRepresents);
-	}
-	var	extendsAccidentTransportation=Object.create(accidentTransportation);
-		extendsAccidentTransportation.problemOccurredAccident='10 km';
-	var civilianAviationAccident= Object.create(extendsAccidentTransportation);
-		civilianAviationAccident.nameAirline='Alitalia';
-		civilianAviationAccident.listTranspotationInvolved=['Boing737' , 'Airbus A319'];
-	var militaryAviationAccidents=Object.create(extendsAccidentTransportation);
-		militaryAviationAccidents.territory='Russia'
-		militaryAviationAccidents.involvedCountries=['France' , 'Armenia' , 'Russia'];
-		militaryAviationAccidents.listTranspotationInvolved=['MIG-29' , 'AH-64 Apach'];
-		Object.getOwnPropertyDescriptor(accidentTransportation,'date');
-		extendsAccidentTransportation.__proto__;
-		militaryAviationAccidents.__proto__;
+        date : Date(),
+        listTransportationInvolved : ['Boing737','Airbus A319','MIG-29'],
+        damagedPeopleNumber : 10,
+        reasonAccident : "The accident happened because the driver of Fiat was drunk.",
+        toString:function(){
+            return 'Crash happened on' + this.date + ' . ' + this.reasonAccident + '.' + 'damaged people number is' + this.DamagedPeopleNumber;
+        }
+    }
+    var p=function(){
+        var m;
+        for(i in transportation){
+            if (m===transportation[i]){
+                accidentTransportation.listTransportationInvolved[listTransportationInvolved.length]=m;
+                return accidentTransportation.listTransportationInvolved;
+            }
+        }
+    }
+    var hasInfo=function(accidentRepresents){
+        for(accidentRepresents in accidentTransportation){
+            return accidentTransportation.hasOwnProperty(accidentRepresents);
+        }
+    }
+    var extendsAccidentTransportation=Object.create(accidentTransportation);
+        extendsAccidentTransportation.problemOccurredAccident='10 km';
+    var civilianAviationAccident= Object.create(extendsAccidentTransportation);
+        civilianAviationAccident.nameAirline='Alitalia';
+        civilianAviationAccident.listTranspotationInvolved=['Boing737' , 'Airbus A319'];
+    var militaryAviationAccidents=Object.create(extendsAccidentTransportation);
+        militaryAviationAccidents.territory='Russia'
+        militaryAviationAccidents.involvedCountries=['France' , 'Armenia' , 'Russia'];
+        militaryAviationAccidents.listTranspotationInvolved=['MIG-29' , 'AH-64 Apach'];
+    var transportation=['Boing737','Airbus A319','MIG-29','AH-64 Apach'];
+        militaryAviationAccidents.damagedPeopleNumber=30;
+        Object.defineProperty(civilianAviationAccident,'confInform',{value:'information' writeble:false});
+        Object.getPrototypeOf(militaryAviationAccidents);
+        militaryAviationAccidents.__proto__;
+
+
